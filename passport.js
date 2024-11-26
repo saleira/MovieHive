e34ed102870335ece576f79b025390608d8e2dc2
saleira <goncalo.caro@gmail.com>
2024-11-26 22:41:50 +0000

    Created passport strategies

---
+++ passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'Username',
+        passwordField: 'Password',
+    },
+    async (username, password, callback) => {
+        try {
+            const user = await Users.findOne({Username: username});
+
+            if(!user) {
+                return callback(null, false, {
+                    message: 'Incorrect username or password.'
+                });
+            }
+            return callback(null, user);
+        } catch (err) {
+            return callback(err);
+        }
+    }
+));
+
+passport.use(new JWTStrategy(
+    {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: 'ultra_secret_key_for_MovieHive_APIs'
+    },
+    async (jwtPayload, callback)  => {
+        try {
+            const user = await Users.findById(jwtPayload._id);
+            return callback(null, user);
+        } catch (err) {
+            return callback(err);
+        }
+    }
+));
