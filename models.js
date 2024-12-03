658f20b4567fe1cda1f4ef47f268c61dadaafebb
saleira <goncalo.caro@gmail.com>
2024-12-03 19:44:41 +0000

    Add hasing to password

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+    bcrypt = require('bcrypt');
 
 const movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -24,6 +25,14 @@ const userSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}],
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 
 const Movie = mongoose.model('Movie', movieSchema);
 const User = mongoose.model('User', userSchema);
