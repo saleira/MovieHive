a79288cb7e549f8bb3fee1b493be4bb5f1bc0357
saleira <goncalo.caro@gmail.com>
2024-12-09 19:48:02 +0000

    New login errors messages

--- auth.js
+++ auth.js
@@ -17,16 +17,17 @@ let generateJWTToken = (user) => {
 module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', {session: false}, (error, user, info) => {
-            if (error||!user) {
-                return res.status(400).json(
-                    {
-                        message: 'Something is not right',
-                        user: user
-                    });
+            if (error) {
+                return res.status(500).json({ message: 'An error occurred during login.', error });
             }
+
+            if (!user) {
+                return res.status(401).json({ message: info.message });
+            }
+
             req.login(user, {session: false}, (error) => {
                 if (error) {
-                    res.send(error);
+                    return res.status(500).json({ message: 'An error occurred during login.', error });
                 }
                 let token = generateJWTToken(user.toJSON());
                 return res.json({user, token});
