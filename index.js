908bdb6621452d9c7a254e5980c156fd1d09995e
saleira <goncalo.caro@gmail.com>
2024-12-18 23:16:46 +0000

    updated cors

--- index.js
+++ index.js
@@ -18,29 +18,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:8080', 'https://movie-hive-ee3949a892be.herokuapp.com/', 'http://localhost:1234', 'https://movie-hive-client.netlify.app/'];
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-};
-app.use(cors(corsOptions));
-
-// const allowedOrigins = ['http://localhost:8080', 'https://movie-hive-ee3949a892be.herokuapp.com/', 'http://localhost:1234'];
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) { 
-//             const message = "The CORS policy for this application doesn't allow access from origin " + origin;
-//             return callback(new Error(message), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+const allowedOrigins = ['http://localhost:8080', 'https://movie-hive-ee3949a892be.herokuapp.com/', 'http://localhost:1234', 'https://movie-hive-client.netlify.app/'];
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                let message =
+                    "The CORS policy for this application doesn’t allow access from origin " +
+                    origin;
+                return callback(new Error(message), false);
+            }
+            return callback(null, true);
+        },
+    })
+);
 
 let auth = require('./auth')(app);
 const passport = require('passport');
